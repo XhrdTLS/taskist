3e2d1bfc591f9789b26fc60bed2575e797c8ca26
Tomás L <tomasalejandrolillosilva@gmail.com>
2024-08-28 13:51:31 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# taskist
+ App móvil creada con Flutter para creación y actualización de tareas diarias.
---
+++ android/app/src/main/kotlin/cl/utem/taskist/MainActivity.kt
@@ -0,0 +1,5 @@
+package cl.utem.taskist
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
